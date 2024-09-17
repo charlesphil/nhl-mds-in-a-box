8276e9cc1daf8c36d41ea2502d895b451f13a5dc
Charlie Phil <71610407+charlesphil@users.noreply.github.com>
2024-09-17 10:58:36 -0400

    schema and alias macros

--- transform/macros/generate_alias_name.sql
+++ transform/macros/generate_alias_name.sql
@@ -1,16 +1,12 @@
 {% macro generate_alias_name(custom_alias_name=none, node=none) -%}
 
-    {%- if custom_alias_name -%}
-
-        {{ custom_alias_name | trim }}
+    {%- if custom_alias_name -%} {{ custom_alias_name | trim }}
 
     {%- elif node.version -%}
 
         {{ return(node.name ~ "_v" ~ (node.version | replace(".", "_"))) }}
 
-    {%- else -%}
-
-        {{ node.name.split('_', 1)[1] }}
+    {%- else -%} {{ node.name.split("_", 1)[1] }}
 
     {%- endif -%}
 
